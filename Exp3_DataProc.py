cdb27549a1477a26803f6ea4692ece770a60e306
Olivia <laiyunghwei@gmail.com>
2023-04-08 15:47:03 +0800

    Update Exp3_DataProc.py

--- Exp3_DataProc.py
+++ Exp3_DataProc.py
@@ -5,53 +5,15 @@
 """
 
 import os
-import jieba
 import json
 import numpy as np
 import torch
-import torch.nn as nn
-from collections import defaultdict
-from Exp3_DataSet import trainset, testset
+from torch.utils.data import Dataset
+from transformers import BertTokenizer
+from Exp3_DataSet import trainset, valset, testset
 from Exp3_Config import Training_Config
 
-stopwords = []
 config = Training_Config()
-relation_to_id, id_to_relation = {}, {}
-
-def get_stopwords():
-    with open("data/stopwords_utf8.txt","r",encoding="utf-8") as file:
-        for char in file:
-            stopwords.append(char.replace("\n",""))
-
-def remove_stopwords(sent_list):
-    new_list = []
-    for char in sent_list:
-        isstopword = False
-        for ch in stopwords:
-            if char == ch:
-                isstopword = True
-                continue
-        if not isstopword:
-            new_list.append(char)
-    return new_list
-
-def add_to_vocab(vocab_list, new_list):
-    for word in new_list:
-        if not word in vocab_list:
-            if len(vocab_list) < config.vocab_size:
-                vocab_list.append(word)
-    return vocab_list
-
-def vectorize_data(sentence, vocab_list):
-    vector = np.zeros(config.embedding_dimension, dtype=int)
-    index = 0
-    new_list = list(jieba.cut(sentence, cut_all=False))
-    new_list = remove_stopwords(new_list)
-    for w in new_list:
-        if w in vocab_list and index < 100:
-            vector[index] = vocab_list.index(w)
-            index += 1
-    return vector
 
 def get_location(head, tail, sentence):
     loc_vector = []
@@ -62,136 +24,203 @@ def get_location(head, tail, sentence):
     loc_vector.append(head_vec)
     loc_vector.append(";")
     loc_vector.append(tail_vec)
-    loc_vector.append(";")
     return loc_vector
 
-def get_relation_id():
+
+def relation2id():
     with open('data/rel2id.json',encoding='utf-8') as json_file:
         data = json.load(json_file)
         relation_dict = data[1]
     return relation_dict
 
-    
 
-def preprocess(dataset=trainset, keyword="train"):
-    print("数据预处理开始......")
-    
-    tmp_list, vocab_list = [], []
-
-    '''Get Relation'''
-    if not os.path.exists("data/relation.txt"):
-        file = open('data/relation.txt','w',encoding='utf-8')
-        for key in relation_to_id.keys():
-            file.write(key+"\n")
-        file.close()
-
-
-    '''Get Vocab List'''
-    if not os.path.exists('data/vocab_list.txt'):
-        for i in range(len(trainset)):
-            if len(tmp_list) > config.vocab_size:
-                break
-            new_list = list(jieba.cut(trainset[i]['text'], cut_all=False))
-            new_list = remove_stopwords(new_list)
-            tmp_list = add_to_vocab(tmp_list, new_list)
-
-        file = open('data/vocab_list.txt','w',encoding='utf-8')
-        for word in tmp_list:
-            file.write(word+"\n")
-        file.close()
-
-    with open('data/vocab_list.txt','r', encoding='utf-8') as file:
-        for line in file:
-            vocab_list.append(line.replace("\n",""))
-
-    print("词汇表:",len(vocab_list))
-    file.close()
-
-
-    '''Get Sentence Vector / Word2Vec'''
-    train_vec = []
-    if not os.path.exists('data/trainX.txt'):
-        for i in range(len(trainset)):
-            vector = vectorize_data(trainset[i]['text'], vocab_list)
-            train_vec.append(vector)
-        train_vec = np.array(train_vec)
-        np.savetxt("data/trainX.txt",train_vec,fmt='%s')
-    
-    train_vec = np.loadtxt("data/trainX.txt")
-    train_vec = np.array(train_vec)
-    print("训练集词嵌入:",train_vec.shape)
-
-
+def head_tail_location(dataset):
     '''Get Head / Tail Location'''
     loc_vec = []
-    if not os.path.exists('data/location_vector.txt'):
-        for i in range(len(trainset)):
-            location = get_location(trainset[i]['head'],
-                                    trainset[i]['tail'], 
-                                    trainset[i]['text'])
-            location.append(relation_to_id[trainset[i]['relation']])
+    if dataset == trainset:
+        key = "trainset"
+    elif dataset == valset:
+        key = "valset"
+    elif dataset == testset:
+        key = "testset"
+    filename = 'data/'+key+'_location.txt'
+
+    if not os.path.exists(filename):
+        for i in range(len(dataset)):
+            location = get_location(dataset[i]['head'],
+                                    dataset[i]['tail'], 
+                                    dataset[i]['text'])
             loc_vec.append(location)
         loc_vec = np.array(loc_vec)
-        np.savetxt("data/location_vector.txt",loc_vec,fmt='%s')
+        np.savetxt(filename,loc_vec,fmt='%s')
+        return loc_vec
     
-    with open("data/location_vector.txt","r") as file:
+    with open(filename,"r") as file:
         for line in file:
             tmp_vec = []
-            loc1, loc2, relation = line.replace("\n","").split(" ; ")
+            loc1, loc2 = line.replace("\n","").split(" ; ")
             loc1 = loc1.strip('][').split(', ')
             loc2 = loc2.strip('][').split(', ')
             loc1 = [int(i) for i in loc1]
             loc2 = [int(i) for i in loc2]
-            relation = int(relation)
             tmp_vec.append(loc1)
             tmp_vec.append(loc2)
-            tmp_vec.append(relation)
             loc_vec.append(tmp_vec)
-    loc_vec = np.array(loc_vec)
-    print("头尾位置标签:",loc_vec.shape)
+    loc_vec = np.array(loc_vec, dtype=int)
+    return loc_vec
+    
+
+class MyTokenizer(object):
+    def __init__(self, pretrained_model_path=None, mask_entity=False):
+        self.pretrained_model_path = pretrained_model_path or 'bert-base-chinese'
+        self.bert_tokenizer = BertTokenizer.from_pretrained(self.pretrained_model_path)
+        self.mask_entity = mask_entity
+
+    def tokenize(self, sentence, loc_vec):
+        pos_head = loc_vec[0]
+        pos_tail = loc_vec[1]
+
+        if pos_head[0] > pos_tail[0]:
+            pos_min = pos_tail
+            pos_max = pos_head
+            rev = True
+        else:
+            pos_min = pos_head
+            pos_max = pos_tail
+            rev = False
+
+        sent0 = self.bert_tokenizer.tokenize(sentence[:pos_min[0]])
+        ent0 = self.bert_tokenizer.tokenize(sentence[pos_min[0]:pos_min[1]])
+        sent1 = self.bert_tokenizer.tokenize(sentence[pos_min[1]:pos_max[0]])
+        ent1 = self.bert_tokenizer.tokenize(sentence[pos_max[0]:pos_max[1]])
+        sent2 = self.bert_tokenizer.tokenize(sentence[pos_max[1]:])
+
+        if rev:
+            if self.mask_entity:
+                ent0 = ['[unused6]']
+                ent1 = ['[unused5]']
+            pos_tail = [len(sent0), len(sent0) + len(ent0)]
+            pos_head = [
+                len(sent0) + len(ent0) + len(sent1),
+                len(sent0) + len(ent0) + len(sent1) + len(ent1)
+            ]
+        else:
+            if self.mask_entity:
+                ent0 = ['[unused5]']
+                ent1 = ['[unused6]']
+            pos_head = [len(sent0), len(sent0) + len(ent0)]
+            pos_tail = [
+                len(sent0) + len(ent0) + len(sent1),
+                len(sent0) + len(ent0) + len(sent1) + len(ent1)
+            ]
+        tokens = sent0 + ent0 + sent1 + ent1 + sent2
+
+        re_tokens = ['[CLS]']
+        cur_pos = 0
+        pos1 = [0, 0]
+        pos2 = [0, 0]
+        for token in tokens:
+            token = token.lower()
+            if cur_pos == pos_head[0]:
+                pos1[0] = len(re_tokens)
+                re_tokens.append('[unused1]')
+            if cur_pos == pos_tail[0]:
+                pos2[0] = len(re_tokens)
+                re_tokens.append('[unused2]')
+            re_tokens.append(token)
+            if cur_pos == pos_head[1] - 1:
+                re_tokens.append('[unused3]')
+                pos1[1] = len(re_tokens)
+            if cur_pos == pos_tail[1] - 1:
+                re_tokens.append('[unused4]')
+                pos2[1] = len(re_tokens)
+            cur_pos += 1
+        re_tokens.append('[SEP]')
+        return re_tokens[1:-1], pos1, pos2
+    
+    
+def convert_pos_to_mask(e_pos, max_len=config.embedding_dimension):
+    e_pos_mask = [0] * max_len
+    for i in range(e_pos[0], e_pos[1]):
+        e_pos_mask[i] = 1
+    return e_pos_mask
 
 
-    '''Preprocess'''
-    if not os.path.exists('data/train_data.jsonl'):
-        data = []
-        keylist = ["h","t","relation","text"]
-        detailkey = ["name","pos"]
-        with open('data/train_data.txt', mode='w', encoding='utf-8') as f:
-            json.dump([], f)
-        for i in range(len(trainset)):
-            tmp_dict = {}
-            tmp_dict = {key: None for key in keylist}
-            tmp_dict["h"] = {key: None for key in detailkey}
-            tmp_dict["t"] = {key: None for key in detailkey}
-            tmp_dict["h"]["name"] = trainset[i]['head']
-            tmp_dict["h"]["pos"] = loc_vec[i][0]
-            tmp_dict["t"]["name"] = trainset[i]['tail']
-            tmp_dict["t"]["pos"] = loc_vec[i][1]
-            tmp_dict["relation"] = trainset[i]['relation']
-            tmp_dict["text"] = trainset[i]['text']
-            data.append(tmp_dict)
+def read_data(dataset, tokenizer=None, max_len=config.embedding_dimension):
+    tokens_list = []
+    e1_mask_list = []
+    e2_mask_list = []
+    tags = []
+    loc_vec = head_tail_location(dataset)
+    if tokenizer is None:
+        tokenizer = MyTokenizer()
+    for i in range(len(dataset)):
+        tokens, pos_e1, pos_e2 = tokenizer.tokenize(dataset[i]['text'],loc_vec[i])
+        if pos_e1[0] < max_len - 1 and pos_e1[1] < max_len and \
+                pos_e2[0] < max_len - 1 and pos_e2[1] < max_len:
+            tokens_list.append(tokens)
+            e1_mask = convert_pos_to_mask(pos_e1, max_len)
+            e2_mask = convert_pos_to_mask(pos_e2, max_len)
+            e1_mask_list.append(e1_mask)
+            e2_mask_list.append(e2_mask)
+            if not dataset == testset:
+                tag = dataset[i]['relation']
+                tags.append(tag)
+    return tokens_list, e1_mask_list, e2_mask_list, tags
+    
+
+class SentenceREDataset(Dataset):
+    def __init__(self, dataset, pretrained_model_path=None, max_len=config.embedding_dimension):
+        self.pretrained_model_path = pretrained_model_path or 'bert-base-chinese'
+        self.tokenizer = MyTokenizer(pretrained_model_path=self.pretrained_model_path)
+        self.max_len = max_len
+        self.tokens_list, self.e1_mask_list, self.e2_mask_list, self.tags = read_data(dataset, tokenizer=self.tokenizer, max_len=self.max_len)
+        self.tag2idx = relation2id()
+        self.dataset = dataset
+
+    def __len__(self):
+        return len(self.tokens_list)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        sample_tokens = self.tokens_list[idx]
+        sample_e1_mask = self.e1_mask_list[idx]
+        sample_e2_mask = self.e2_mask_list[idx]
+        encoded = self.tokenizer.bert_tokenizer.encode_plus(sample_tokens, max_length=self.max_len, pad_to_max_length=True)
+        sample_token_ids = encoded['input_ids']
+        sample_token_type_ids = encoded['token_type_ids']
+        sample_attention_mask = encoded['attention_mask']
+
+        if self.dataset == testset:
+            sample = {
+                'token_ids': torch.tensor(sample_token_ids),
+                'token_type_ids': torch.tensor(sample_token_type_ids),
+                'attention_mask': torch.tensor(sample_attention_mask),
+                'e1_mask': torch.tensor(sample_e1_mask),
+                'e2_mask': torch.tensor(sample_e2_mask)
+            }
+        else:
+            sample_tag = self.tags[idx]
+            sample_tag_id = self.tag2idx[sample_tag]
+            sample = {
+                'token_ids': torch.tensor(sample_token_ids),
+                'token_type_ids': torch.tensor(sample_token_type_ids),
+                'attention_mask': torch.tensor(sample_attention_mask),
+                'e1_mask': torch.tensor(sample_e1_mask),
+                'e2_mask': torch.tensor(sample_e2_mask),
+                'tag_id': torch.tensor(sample_tag_id)
+            }
         
-        with open('data/train_data.jsonl', mode='w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=2)
-
-    data = []
-    with open('data/train_data.jsonl', 'r', encoding='utf-8') as fp:
-        data = json.load(fp)
-    
-
-    
-
-    
-
-    '''inp = torch.tensor(train_vec[0]).long()
-    embedding = nn.Embedding(config.vocab_size,config.embedding_dimension)'''
-    
+        return sample
 
 
-    print("数据预处理完毕！")
+print("数据预处理开始......")
+print("预处理前的训练集、验证集、测试集大小:",len(trainset),",",len(valset),",",len(testset)) 
+train_data = SentenceREDataset(trainset)
+val_data = SentenceREDataset(valset)
+test_data = SentenceREDataset(testset)
+print("预处理后的训练集、验证集、测试集大小:",len(train_data),",",len(val_data),",",len(test_data)) 
+print("数据预处理完毕！")
 
 
-
-get_stopwords()
-relation_to_id = get_relation_id()
-preprocess()
