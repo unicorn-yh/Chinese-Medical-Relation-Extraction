90a0ad5c35ec6035a52502709db15f9876516a5f
Olivia <laiyunghwei@gmail.com>
2023-04-16 00:52:33 +0800

    Update Exp3_Model.py

--- Exp3_Model.py
+++ Exp3_Model.py
@@ -1,11 +1,10 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as functions
-from transformers import BertTokenizer, BertModel
+
 
 class TextCNN_Model(nn.Module):
 
-    def __init__(self, configs):
+    def __init__(self, configs,pre_embedding):
         super(TextCNN_Model, self).__init__()
 
         vocab_size = configs.vocab_size
@@ -13,60 +12,35 @@ class TextCNN_Model(nn.Module):
         label_num = configs.label_num
 
         # 词嵌入和dropout
-        self.embed = nn.Embedding(vocab_size, embedding_dimension)
+        self.pre_embed = nn.Embedding.from_pretrained(pre_embedding,freeze=False)
+        self.sent_embed = nn.Embedding(vocab_size+2, embedding_dimension)
+        self.pos1_embed = nn.Embedding(153, 10)
+        self.pos2_embed = nn.Embedding(153, 10)
         self.dropout = nn.Dropout(configs.dropout)
+        self.covns = nn.ModuleList([nn.Sequential(nn.Conv1d(in_channels=embedding_dimension+20,
+									out_channels=128,
+									kernel_size=k),nn.Tanh(),nn.MaxPool1d(kernel_size=98-k+1)) for k in range(2,6)])
+        self.linear = nn.Linear(128*4,label_num)
 
-    def forward(self, sentences):
-        print(sentences)
-        return 0
+    def forward(self, x):
+        sent = x['text']
+        pos1 = x['pos1']
+        pos2 = x['pos2']        
+        sent_embed = self.pre_embed(sent)
+        
+        pos1_embed = self.pos1_embed(pos1)
+        pos2_embed = self.pos2_embed(pos2)
+
+        input = torch.cat([sent_embed,pos1_embed,pos2_embed],dim=2) #[64,100,120]
+        input = input.permute(0,2,1) #[64,120,100]
+        input = self.dropout(input)
+        input = [conv(input) for conv in self.covns] #4
+        output = torch.cat(input,dim=1) #[64,512,1]
+        output = self.dropout(output)
+        output = output.view(-1,output.size(1)) #[64,512]
+        output = self.dropout(output)
+        output = self.linear(output) #[64,44]
+        return output
     
 
-class SentenceRE(nn.Module):
 
-    def __init__(self, configs):
-        super(SentenceRE, self).__init__()
-
-        self.pretrained_model_path = configs.pretrained_model_path or 'bert-base-chinese'
-        self.embedding_dimension = configs.embedding_dimension
-        self.dropout = configs.dropout
-        self.class_num = configs.class_num
-
-        self.bert_model = BertModel.from_pretrained(self.pretrained_model_path)
-
-        self.dense = nn.Linear(self.embedding_dimension, self.embedding_dimension)
-        self.drop = nn.Dropout(self.dropout)
-        self.activation = nn.Tanh()
-        self.norm = nn.LayerNorm(self.embedding_dimension * 3)
-        self.hidden2tag = nn.Linear(self.embedding_dimension * 3, self.class_num)
-
-    def forward(self, token_ids, token_type_ids, attention_mask, e1_mask, e2_mask):
-        sequence_output, pooled_output = self.bert_model(input_ids=token_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, return_dict=False)
-
-        # 每个实体的所有token向量的平均值
-        e1_h = self.entity_average(sequence_output, e1_mask)
-        e2_h = self.entity_average(sequence_output, e2_mask)
-        e1_h = self.activation(self.dense(e1_h))
-        e2_h = self.activation(self.dense(e2_h))
-
-        # [cls] + 实体1 + 实体2
-        concat_h = torch.cat([pooled_output, e1_h, e2_h], dim=-1)
-        concat_h = self.norm(concat_h)
-        logits = self.hidden2tag(self.drop(concat_h))
-
-        return logits
-
-    @staticmethod
-    def entity_average(hidden_output, e_mask):
-        """
-        Average the entity hidden state vectors (H_i ~ H_j)
-        :param hidden_output: [batch_size, j-i+1, dim]
-        :param e_mask: [batch_size, max_seq_len]
-                e.g. e_mask[0] == [0, 0, 0, 1, 1, 1, 0, 0, ... 0]
-        :return: [batch_size, dim]
-        """
-        e_mask_unsqueeze = e_mask.unsqueeze(1)  # [b, 1, j-i+1]
-        length_tensor = (e_mask != 0).sum(dim=1).unsqueeze(1)  # [batch_size, 1]
-
-        sum_vector = torch.bmm(e_mask_unsqueeze.float(), hidden_output).squeeze(1)  # [b, 1, j-i+1] * [b, j-i+1, dim] = [b, 1, dim] -> [b, dim]
-        avg_vector = sum_vector.float() / length_tensor.float()  # broadcasting
-        return avg_vector
